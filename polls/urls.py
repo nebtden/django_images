aa61bf71f7b2c52636056e52b5730c13d395b19b
simon <nebtden@gmail.com>
2018-07-23 12:46:48 +0800

    路由 管理

--- polls/urls.py
+++ polls/urls.py
@@ -3,7 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('tt', views.test, name='test'),
+    path('tt', views.test, name='tt'),
     path('', views.index, name='index'),
+    path('detail/<int:question_id>', views.detail, name='testid'),
+
 
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    questions = Question.objects.all()
+    data = ','.join([q.question_text for q in questions])
+    return HttpResponse(data)
+
 
 def test(request):
     return HttpResponse("Hello, world. You're at the polls test.")
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
