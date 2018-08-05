ce03154fb622253d211502e6a961a30001ff1a64
simon <nebtden@gmail.com>
2018-08-05 21:56:49 +0800

    setting 添加

--- begin.cmd
+++ begin.cmd
@@ -1,5 +1,6 @@
 D:  && cd \python\env && Scripts\activate
-D:  && cd \python && django_env\Scripts\activate
+D:  && cd \python && env\Scripts\activate && cd django_images
+D:  && cd \python && django_env\Scripts\activate && cd django_images
 
 C:\Users\z\Anaconda3\Scripts\activate django
 C:\Users\z\Anaconda3\Scripts\deactivate
